c30ded7c52f52222713f49184673258c27628d27
radomirKrastev <radomirkostantinov@gmail.com>
2020-03-04 18:25:44 +0200

    REST services and AJAX Exercise problems 3 and 4

--- REST Services and AJAX/Exercise/03.PHONEBOOK/app.js
+++ REST Services and AJAX/Exercise/03.PHONEBOOK/app.js
@@ -1,5 +1,55 @@
 function attachEvents() {
-    console.log('TODO...');
+  const createButton = document.querySelector("#btnCreate");
+  const loadButton = document.querySelector("#btnLoad");
+  const phonebook = document.querySelector("#phonebook");
+  const person = document.querySelector("#person");
+  const phone = document.querySelector("#phone");
+
+  function updatePhonebook(data) {
+    let keys = Object.keys(data);
+    phonebook.innerHTML = "";
+
+    for (let key of keys) {
+      let deleteButton = document.createElement("button");
+      deleteButton.innerHTML = "Delete";
+      deleteButton.addEventListener("click", deleteContact);
+
+      let li = document.createElement("li");
+      li.innerHTML = `${data[key].person}: ${data[key].phone}`;
+      li.id = key;
+      li.appendChild(deleteButton);
+      phonebook.appendChild(li);
+    }
+  }
+
+  function loadEntries() {
+    fetch("https://phonebook-nakov.firebaseio.com/phonebook.json")
+      .then((x) => x.json())
+      .then(updatePhonebook);
+  }
+
+  function deleteContact(e) {
+    let contact = e.target.parentElement.id;
+    phonebook.removeChild(phonebook.querySelector(`#${contact}`));
+
+    fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${contact}.json`, {
+      method: "DELETE"
+    });
+  }
+
+  function createContact() {
+    fetch(`https://phonebook-nakov.firebaseio.com/phonebook.json`, {
+      method: "POST",
+      body: JSON.stringify({ person: person.value, phone: phone.value })
+    });
+
+    loadEntries();
+    person.value = "";
+    phone.value = "";
+  }
+
+  loadButton.addEventListener("click", loadEntries);
+  createButton.addEventListener("click", createContact);
 }
 
 attachEvents();
--- REST Services and AJAX/Exercise/04.MESSENGER/app.js
+++ REST Services and AJAX/Exercise/04.MESSENGER/app.js
@@ -1,5 +1,31 @@
 function attachEvents() {
-    console.log('TODO...');
+  const author = document.querySelector("#author");
+  const content = document.querySelector("#content");
+  const submitButton = document.querySelector("#submit");
+  const refreshButton = document.querySelector("#refresh");
+  const messages = document.querySelector("#messages");
+
+  function processData(data) {
+    let keys = Object.keys(data);
+    messages.innerHTML = "";
+    messages.innerHTML = keys.map((x) => `${data[x].author}: ${data[x].content}`).join("\n");
+  }
+
+  function showMessages() {
+    fetch("https://rest-messanger.firebaseio.com/messanger.json")
+      .then((x) => x.json())
+      .then(processData);
+  }
+
+  function addMessage() {
+    fetch("https://rest-messanger.firebaseio.com/messanger.json", {
+      method: "POST",
+      body: JSON.stringify({ author: author.value, content: content.value })
+    });
+  }
+
+  refreshButton.addEventListener("click", showMessages);
+  submitButton.addEventListener("click", addMessage);
 }
 
 attachEvents();
