a416d05ebf4b626a295fcfdeb32be6c73341e925
radomirKrastev <radomirkostantinov@gmail.com>
2020-03-06 10:07:08 +0200

    Lab problem 1

--- Asynchronous Programming/Lab/01.GITHUB-COMMITS/app.js
+++ Asynchronous Programming/Lab/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,47 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+  const html = {
+    username: () => document.querySelector("#username"),
+    repo: () => document.querySelector("#repo"),
+    commits: () => document.querySelector("#commits")
+  };
+
+  function displayError(message) {
+    html.commits().innerHTML = "";
+    const li = document.createElement("li");
+    li.innerHTML = message;
+    html.commits().appendChild(li);
+  }
+
+  function displayCommits(data) {
+    const fragment = document.createDocumentFragment();
+    html.commits().innerHTML = "";
+
+    data.forEach(function(x) {
+      console.log(x);
+      const li = document.createElement("li");
+      li.innerHTML = `${x.commit.author.name}: ${x.commit.message}`;
+      fragment.appendChild(li);
+    });
+
+    html.commits().appendChild(fragment);
+  }
+
+  async function getData() {
+    try {
+      const response = await fetch(
+        `https://api.github.com/repos/${html.username().value}/${html.repo().value}/commits`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} (${response.statusText})`);
+      }
+
+      const result = await response.json();
+      displayCommits(result);
+    } catch (e) {
+      displayError(e.message);
+    }
+  }
+
+  getData();
 }
